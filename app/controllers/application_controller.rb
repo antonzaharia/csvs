0d845f9b1257557a435cd61266b97ae948fecd30
Anton Zaharia <zaharia_anton@yahoo.com>
2022-11-23 17:43:54 +0000

    nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= set_current_user
+  end
+
+  def set_current_user
+    @current_user = logged_in? ? User.find(session[:user_id]) : nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
