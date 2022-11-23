2bc742d13c3dbf0546f9005708e4e203082c2699
Anton Zaharia <zaharia_anton@yahoo.com>
2022-11-23 18:17:46 +0000

    users signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get :dashboard, to: 'home#dashboard'
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 end
