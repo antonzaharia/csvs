742b3dcb95d5f1a13dd827ef679b82fa4d121be6
Anton Zaharia <zaharia_anton@yahoo.com>
2022-11-23 18:39:14 +0000

    sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: user_params[:email])
+    if @user&.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+
+      redirect_to dashboard_path
+    else
+      redirect_to new_session_path, error: 'Wrong email or password.'
+    end
   end
 
   def destroy
     session.delete(:user_id)
 
     redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
